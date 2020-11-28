6e708668e9e77d32ff75a157120ced51e8a99f02
morteza <khodemousa@gmail.com>
2020-11-28 13:22:41 +0330

    migrate CustomUser as default UserModel & added git.log file

--- djang_users/settings.py
+++ djang_users/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#Custom settings
+
+#این مدل جایگزین مدل یوزر است.
+#appname.ModelName
+#توجه شود که به فایل model اشاره نمی شود
+AUTH_USER_MODEL='users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    phone=models.CharField(max_length=11)
+    location=models.CharField(max_length=127)
+    bio=models.TextField()
+
