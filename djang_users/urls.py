927a5b87c1fc8b454e8e4a4d53c89e4b7185357b
morteza <khodemousa@gmail.com>
2020-11-28 15:52:07 +0330

    fixed login

--- djang_users/urls.py
+++ djang_users/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import render
+from django.urls import path, include
+
+def home(request):
+    return render(request,'_base.html',{})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name = 'home'),
+path('accounts/', include('users.urls')),
 ]
+
+
