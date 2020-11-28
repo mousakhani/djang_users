86d244cd4cd79688fe1eea55212bd2db0e9f975a
morteza <khodemousa@gmail.com>
2020-11-28 14:17:19 +0330

    added customuser to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class AdminCustomUser(admin.ModelAdmin):
+    list_display = ['username','first_name','last_name','phone','email']
--- users/models.py
+++ users/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    phone=models.CharField(max_length=11)
+    phone=models.CharField(max_length=11, unique=True)
     location=models.CharField(max_length=127)
     bio=models.TextField()
 
+    # USERNAME_FIELD =('phone')
+    # REQUIRED_FIELDS = ['phone']
